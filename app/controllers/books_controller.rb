b0f1b96021e23b6022a74aeb60e07747c30638e9
EC2 Default User <ec2-user@ip-172-31-28-160.ap-northeast-1.compute.internal>
2022-12-14 12:43:35 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,52 @@
 class BooksController < ApplicationController
-  def new
-    @book=Book.new
-  end
 
   def create
-    @book=Book.new(book_params)
+    @new_book=Book.new(book_params)
     @book.user_id=current_user.id
     @book.save
     redirect_to book_path(@book)
   end
 
   def index
+    @user=current_user
+    @book=Book.new
+    @books=Book.all
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def show
+    @book_new=Book.new
+    @book=Book.find(params[:id])
+    @user=@book.user
   end
 
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
     redirect_to books_path
+  end
+
+  def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully update."
+      redirect_to book_path(@book)
+    else
+    render :edit
+    end
+  end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
-  end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: "homes#top"
-  resources :books, only:[:new, :create, :index, :show, :destroy]
+  resources :books, only:[:create, :index, :show, :destroy]
   resources :users, only:[:show, :edit]
   get "/homes/about"=>"homes#about", as: "about"
 
